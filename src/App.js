c5c681006339db22c3e912b4047456d3a61d7aff
arifa-akter <arifaakterchoity@gmail.com>
2022-05-05 00:28:32 +0600

    add header part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Share/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className="text-danger">hello my cars inventory </h1>
+    <div>
+    <Header></Header>
+        
     </div>
   );
 }
---
+++ src/components/Share/Header/Header.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import profile from '../../../images/profile.png'
+import './Header.css'
+const Header = () => {
+    return (
+  <section className='nav-design'>
+               <section className='top-header'>
+            <div className="container">
+               <div className="row">
+                 <div className="col-12 col-lg-7 login-language ">
+                  <div className="main-item d-flex justify-content-center d-lg-flex justify-content-lg-start">
+                  <div className="login-language-main">
+                        <img src={profile} alt="" />
+                        <span>Login</span>
+                        <span>Sign up</span>
+                       
+                        <span>
+                        <i className="fa fa-globe" aria-hidden="true"></i>Language</span>
+                   </div>
+                  </div>
+                 </div>
+                 <div className="col-lg-5 contact d-lg-block d-none login-language-main">
+              
+                 <span>
+                 <i className="fa fa-phone" aria-hidden="true"></i>+98765340753</span>  
+                 
+                 <span>
+                 <i className="fa fa-map-marker" aria-hidden="true"></i>nurjahn road dhaka</span> 
+                 </div>
+               </div>
+            </div>
+          </section>
+      <Navbar collapseOnSelect expand="lg"  variant="dark" className="nav-p-none">
+  <Container>
+  <Navbar.Brand as={Link} to="/home">
+      {/* <img style={{width:'50px', height:'50px'}} alt="" /> */}
+      <span className="menu-title">arifa</span>
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ms-auto nav-bg">
+      <Nav.Link as={Link} to="/"><span className="menu-style">HOME</span></Nav.Link>
+      <Nav.Link as={Link} to="/"><span className="menu-style">ABOUT</span></Nav.Link>
+      <Nav.Link href="#deets"><span className="menu-style">MANAGE INVENTORY</span></Nav.Link>
+      <Nav.Link as={Link} to="" href="" >
+      <span className="menu-style">ADD ITEM</span>
+      </Nav.Link>
+      <Nav.Link as={Link} to="">
+      <span className="menu-style">MY ITEM</span>
+      </Nav.Link>
+      <Nav.Link as={Link} to=""><span className="menu-style">BLOGS</span></Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+  </section>
+    
+    );
+};
+
+export default Header;
