441cb938bbb89495bed25eaff8d406af057de216
arifa-akter <arifaakterchoity@gmail.com>
2022-05-07 00:40:27 +0600

    add extra part in home

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import CarInventory from '../CarInventory/CarInventory';
+import Operation from '../Operation/Operation';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <CarInventory></CarInventory>
+           <Operation></Operation>
         </div>
     );
 };
---
+++ src/components/Home/Operation/Operation.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Operation.css'
+const Operation = () => {
+    return (
+      <section className='operation'>
+           <div className="container">
+               <div className="row">
+                   <h3 className='text-center extra-color'>what are our Hours of operation</h3>
+                   <div className="col-lg-6 col-12">
+                       <h2 className='text-center extra-color'>Sales Department</h2>
+                        <p className='text-center extra-color'><span>Mon:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>Tus:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>wed:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>thur:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>fri:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>sat:</span>	8:00am - 5:00pm</p>
+                    </div>
+                    <div className="col-lg-6 col-12">
+                        <h2 className='text-center extra-color'>Service Department</h2>
+                        <p className='text-center extra-color'><span>Mon:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>Tus:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>wed:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>thur:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>fri:</span>	8:00am - 5:00pm</p>
+                        <p className='text-center extra-color'><span>sat:</span>	8:00am - 5:00pm</p>
+                    </div>
+                   </div>
+           </div>
+      </section>
+    );
+};
+
+export default Operation;
